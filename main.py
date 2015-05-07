d1419e9b4338bdc64a7689fef72e3517f6383b91
Sim Tze Jan <tzejan@gmail.com>
2015-05-07 10:48:16 +0100

    Now the echo server responds with the HTTP verb called

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
-print "Well done!"
+import webapp2
+
+
+class DefaultHandler(webapp2.RequestHandler):
+    def respond(self, httpVerb):
+        self.response.write('HTTP verb is %s' % httpVerb)
+
+    def get(self):        
+        self.respond('GET')
+
+    def post(self):        
+        self.respond('POST')
+
+    def put(self):        
+        self.respond('PUT')
+
+    def delete(self):
+        self.respond('DELETE')
+
+
+
+routes = [
+    (r'/', DefaultHandler),
+]
+
+config = {}
+
+
+app = webapp2.WSGIApplication(routes=routes, debug=True, config=config)
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
+
